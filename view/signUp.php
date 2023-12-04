35401c62546b80a8252a7c3cbf621c8806844e57
khary <khaithph39205@fpt.edu.vn>
2023-12-04 10:57:12 +0700

    sửa

--- view/signUp.php
+++ view/signUp.php
@@ -8,7 +8,7 @@
                 </a>
                 <img src="./assets/img/auth/intro.svg" alt="" class="auth__intro-img" />
                 <p class="auth__intro-text">
-                    The best of luxury brand values, high quality products, and innovative services
+                sản phẩm chất lượng cao và dịch vụ sáng tạo
                 </p>
                 <button class="auth__intro-next d-none d-md-flex js-toggle" toggle-target="#auth-content">
                     <img src="./assets/img/auth/intro-arrow.svg" alt="" />
@@ -22,8 +22,8 @@
                         <img src="./assets/icons/logo.svg" alt="grocerymart" class="logo__img" />
                         <h1 class="logo__title">grocerymart</h1>
                     </a>
-                    <h1 class="auth__heading">Sign Up</h1>
-                    <p class="auth__desc">Let’s create your account and Shop like a pro and save money.</p>
+                    <h1 class="auth__heading">Đăng Nhập</h1>
+                    <p class="auth__desc">Hãy tạo tài khoản và cùng nhau trải nghiệm mua sắm thôi.</p>
                     <form action="./?act=checkSignUp" class="form auth__form" method="post">
                         <div class="form__group">
                             <div class="form__text-input">
@@ -39,7 +39,7 @@
                                 <img src="./assets/icons/message.svg" alt="" class="form__input-icon" />
                                 <img src="./assets/icons/form-error.svg" alt="" class="form__input-icon-error" />
                             </div>
-                            <p class="form__error">Email is not in correct format</p>
+                            <p class="form__error">Email không đúng</p>
                         </div>
                         <div class="form__group">
                             <div class="form__text-input">
@@ -55,7 +55,7 @@
                                 <img src="./assets/icons/lock.svg" alt="" class="form__input-icon" />
                                 <img src="./assets/icons/form-error.svg" alt="" class="form__input-icon-error" />
                             </div>
-                            <p class="form__error">Password must be at least 6 characters</p>
+                            <p class="form__error">Mật khẩu phải có ít nhất 6 ký tự</p>
                         </div>
                         <div class="form__group">
                             <div class="form__text-input">
@@ -71,26 +71,26 @@
                                 <img src="./assets/icons/lock.svg" alt="" class="form__input-icon" />
                                 <img src="./assets/icons/form-error.svg" alt="" class="form__input-icon-error" />
                             </div>
-                            <p class="form__error">Password must be at least 6 characters</p>
+                            <p class="form__error">Mật khẩu phải có ít nhất 6 ký tự</p>
                         </div>
                         <div class="form__group form__group--inline">
                             <label class="form__checkbox">
                                 <input type="checkbox" name="checkbox" id="checkbox" class="form__checkbox-input d-none" />
-                                <span class="form__checkbox-label">Set as default card</span>
+                                <span class="form__checkbox-label">đặt làm thẻ</span>
                             </label>
                         </div>
                         <div class="form__group auth__btn-group">
-                            <button class="btn btn--primary auth__btn form__submit-btn">Sign Up</button>
+                            <button class="btn btn--primary auth__btn form__submit-btn">Đăng Nhập</button>
                             <button class="btn btn--outline auth__btn btn--no-margin">
                                 <img src="./assets/icons/google.svg" alt="" class="btn__icon icon" />
-                                Sign in with Google
+                                Đăng nhập với Google
                             </button>
                         </div>
                     </form>
 
                     <p class="auth__text">
-                        You have an account yet?
-                        <a href="./sign-in.html" class="auth__link auth__text-link">Sign In</a>
+                        Bạn có tải khoản ?
+                        <a href="./sign-in.html" class="auth__link auth__text-link">Đăng kí</a>
                     </p>
                 </div>
             </div>
